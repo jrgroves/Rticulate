a2d7c4b6bda3f5309dbd3ee5529b733f6ea2b735
Jeremy R. Groves <jgroves@niu.edu>
2025-10-20 12:25:14 -0500

    Finalize IEA presentation

--- Build/Code/Lectures Codes.R
+++ Build/Code/Lectures Codes.R
@@ -12,34 +12,17 @@ library(tidyverse)
 library(sf)
 library(usethis)
 
-
-
-
-
-
-
 ## Gretting the Data
  #  usethis::use_course("https://github.com/jrgroves/Rticulate/archive/master.zip", destdir = "./")
 
-    
-    
-    
-    
 ## Getting the Data   
       
     ufo <- read.csv("./build/input/ufo.csv")
 
     load("./build/input/census.RData")
-  
-    
-    
-      
+
     st_as_sf(census)  
 
-    
-    
-    
-    
 ## Observing the Mess
     
     head(ufo)
@@ -58,108 +41,118 @@ library(usethis)
       filter(state != "")
 
     
-    
-ufo.us <- ufo.us %>%
-  select(state, year) %>%
-  mutate(sight = 1) %>%
-  pivot_wider(id_cols = state, names_from = year, names_prefix = "YR",
-              values_from = sight, values_fn = sum)
+  ufo.us <- ufo.us %>%
+    select(state, year) %>%
+    mutate(sight = 1) %>%
+    pivot_wider(id_cols = state, names_from = year, names_prefix = "YR",
+                values_from = sight, values_fn = sum)
+
+## Use GEOID and Abbreviation Bridge to join UFO to Census Data
+  load("./build/input/bridge.RData")
+
+  core <- ufo.us %>%
+    left_join(., bridge, by = c("state" = "Abbr"))%>%
+    left_join(., census, by="GEOID") %>%
+    filter(!is.na(State))
 
 
+## Modifying the variables for better display and percentage change.
 
-
-load("./build/input/bridge.RData")
-
-core <- ufo.us %>%
-  left_join(., bridge, by = c("state" = "Abbr"))%>%
-  left_join(., census, by="GEOID") %>%
-  filter(!is.na(State))
-
-
-## Finding the believers
-
-core2 <- core %>%
-  mutate(pop_2000 = pop_2000/1000000,
-         pop_2010 = pop_2010/1000000) %>%
-  mutate(spp_2000 = YR2000/pop_2000,
-         spp_2010 = YR2010/pop_2010,
-         delta_spp = ((spp_2010 - spp_2000)/spp_2000)) %>%
-  filter(GEOID != "15") %>%
-  st_as_sf()
+  core2 <- core %>%
+    mutate(pop_2000 = pop_2000/1000000,
+           pop_2010 = pop_2010/1000000) %>%
+    mutate(spp_2000 = YR2000/pop_2000,
+           spp_2010 = YR2010/pop_2010,
+           delta_spp = ((spp_2010 - spp_2000)/spp_2000)) %>%
+    filter(GEOID != "15") %>%
+    st_as_sf()
 
 ##Visualization
 
-g1 <- ggplot(core2) +
+  ## Creation of 2000 Plot - Simple
+      ggplot(core2) +
+        geom_sf(aes(fill = spp_2000)) 
+      
+  ## Creation of 2000 Plot - some modifications
+      ggplot(core2) +
+        geom_sf(aes(fill = spp_2000)) +
+        scale_fill_gradient(low = "#e8f4f8", high = "#72bcd4", na.value="white" ) +
+        labs(title = "UFO Sightings per 1 Million Persons in 2000",
+             fill = "Sightings") +
+        theme_bw()
+  
+  ## Creation of 2000 Plot - more modifications and save
+      g1 <- ggplot(core2) +
         geom_sf(aes(fill = spp_2000)) +
         scale_fill_gradient(low = "#e8f4f8", high = "#72bcd4", na.value="white" ) +
         labs(title = "UFO Sightings per 1 Million Persons in 2000",
              fill = "Sightings") +
         theme_bw() +
-        theme(legend.position="bottom",
-              axis.text.x = element_blank(),
-              axis.text.y = element_blank(),
-              panel.grid.major = element_blank(),
-              #panel.border = element_blank(),
-              axis.ticks = element_blank())
+          theme(legend.position="bottom",
+                axis.text.x = element_blank(),
+                axis.text.y = element_blank(),
+                panel.grid.major = element_blank(),
+                #panel.border = element_blank(),
+                axis.ticks = element_blank())
+  
+  ## Creation of 2010 Plot - more modifications and save
+    g2 <- ggplot(core2) +
+            geom_sf(aes(fill = spp_2010)) +
+            scale_fill_gradient(low = "#e8f4f8", high = "#72bcd4", na.value="white" ) +
+            labs(title = "UFO Sightings per 1 Million Persons in 2010",
+                 fill = "Sightings") +
+            theme_bw() +
+            theme(legend.position="bottom",
+                  axis.text.x = element_blank(),
+                  axis.text.y = element_blank(),
+                  panel.grid.major = element_blank(),
+                  #panel.border = element_blank(),
+                  axis.ticks = element_blank())
 
-
-g2 <- ggplot(core2) +
-        geom_sf(aes(fill = spp_2010)) +
-        scale_fill_gradient(low = "#e8f4f8", high = "#72bcd4", na.value="white" ) +
-        labs(title = "UFO Sightings per 1 Million Persons in 2010",
-             fill = "Sightings") +
-        theme_bw() +
-        theme(legend.position="bottom",
-              axis.text.x = element_blank(),
-              axis.text.y = element_blank(),
-              panel.grid.major = element_blank(),
-              #panel.border = element_blank(),
-              axis.ticks = element_blank())
-
-
-
-install.packages("cowplot")
-library(cowplot)
-plot_grid(g1, g2, nrow = 1)
-
-
-ggplot(core2) +
-  geom_sf(aes(fill = delta_spp)) +
-  scale_fill_gradient(low = "#e8f4f8", high = "#72bcd4", na.value="white" ) +
-  labs(title = "Percentage Change in UFO sightings per 1 Million Person between 1990 and 2010",
-       fill = "Sightings") +
-  theme_bw() +
-  theme(legend.position="bottom",
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        panel.grid.major = element_blank(),
-        #panel.border = element_blank(),
-        axis.ticks = element_blank())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  ## Use cowplot to display both graphson same image
+      install.packages("cowplot")
+      library(cowplot)
+      
+      plot_grid(g1, g2, nrow = 1)
+  
+  ## Creation of Percentage Change Plot - more modifications and save
+    g3 <- ggplot(core2) +
+      geom_sf(aes(fill = delta_spp)) +
+      scale_fill_gradient(low = "#e8f4f8", high = "#72bcd4", na.value="white" ) +
+      labs(title = "Percentage Change in UFO sightings per 1 Million Person between 1990 and 2010",
+           fill = "Sightings") +
+      theme_bw() +
+      theme(legend.position="bottom",
+            axis.text.x = element_blank(),
+            axis.text.y = element_blank(),
+            panel.grid.major = element_blank(),
+            #panel.border = element_blank(),
+            axis.ticks = element_blank())
+  
+  ggsave(g3, file = "./Figure One.png", dpi = 300)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
